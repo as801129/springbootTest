0fbc76776296fca9848fa465be580544e30a64dd
DI <DI@host.docker.internal>
2022-05-22 11:22:18 +0800

    qqqq

--- src/main/java/com/crud/ssss.java
+++ src/main/java/com/crud/ssss.java
@@ -4,7 +4,7 @@ public class ssss {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("asdsad44aa");
+		System.out.println("asdsasssd44aa");
 	}
 
 }
