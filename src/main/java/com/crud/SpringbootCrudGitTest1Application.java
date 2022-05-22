d204e6ac965b2bf1e69cfce6281c1b1301a1f14a
DI <DI@host.docker.internal>
2022-05-22 11:19:57 +0800

    first commit

---
+++ src/main/java/com/crud/SpringbootCrudGitTest1Application.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudGitTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudGitTest1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/crud/ssss.java
@@ -0,0 +1,10 @@
+package com.crud;
+
+public class ssss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdsad44aa");
+	}
+
+}
