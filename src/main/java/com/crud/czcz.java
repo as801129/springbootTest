0e93d8f080a15009688a1cff3cca7aec4a67ad87
SHAODI\DI <zz816697@yahoo.com.tw>
2024-04-27 02:55:29 +0800

    aaa

---
+++ src/main/java/com/crud/czcz.java
@@ -0,0 +1,11 @@
+package com.crud;
+
+public class czcz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("asdadads");
+		System.out.print("asdadads");
+	}
+
+}
